829841eea6a81e2a33c667016f62b3934526bcbe
VitaliiPid <vitalii.piddubnyi@gmail.com>
2023-11-28 08:55:08 +0100

    adding a problem statement

--- src/lesson_13/Homework/Fruit.java
+++ src/lesson_13/Homework/Fruit.java
@@ -2,7 +2,11 @@ package lesson_13.Homework;
 
 public class Fruit {
     public static void main(String[] args) {
-
+/*-Cоздать массив строк fruits (фрукты) из 4 элементов: Orange,Apple,Banana,Mango.
+-Вывести созданный массив на экран используя цикл for.
+-Заменить в созданном массиве значение первого элемента на Kiwi (т.е. присвоить первому элементу значение Kiwi)
+-Вывести измененный массив на экран используя цикл for.
+ */
         String[] fruits = {"Orange", "Apple", "Banana", "Mango"};
 
         System.out.println("Исходный массив:");
@@ -16,7 +20,9 @@ public class Fruit {
         for (int i = 0; i < fruits.length; i++) {
             System.out.println(fruits[i]);
         }
-
+/*Используя цикл for вывести на экран все четные элементы массива fruits из первой задачи.
+Задание можно выполнить непосредственно в методе main либо создать отдельный метод.
+*/
         System.out.println("\nЧетные элементы массива:");
         for (int i = 0; i < fruits.length; i++) {
 
